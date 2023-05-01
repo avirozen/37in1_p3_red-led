44ddd7899ace5906c98b3d79245d8a01682506f6
avirozen <117524971+avirozen@users.noreply.github.com>
2023-05-01 13:19:46 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 let val = 0
 led.enable(false)
 basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(1000)
+})
+basic.forever(function () {
     while (val < 1024) {
         val = val + 1
         pins.analogWritePin(AnalogPin.P0, val)
